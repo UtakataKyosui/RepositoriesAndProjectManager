0d142241e4435ac68d54d657575bbba862082174
UtakataKyosui <fill.ayaextech@gmail.com>
2026-01-28 07:44:30 +0900

    fix: 動的レンダリング指定の追加

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -4,6 +4,9 @@ import prisma from "@/lib/prisma";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
+export const dynamic = 'force-dynamic';
+
+
 export default async function AdminDashboard() {
     const session = await getSession();
 
