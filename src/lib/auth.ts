84342b9121d5dc831380b8c250d18be544109ecd
UtakataKyosui <fill.ayaextech@gmail.com>
2026-01-28 07:20:35 +0900

    こっちでよかったかも

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -7,7 +7,7 @@ export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "postgresql",
     }),
-    baseURL: process.env.NEXT_PUBLIC_VERCEL_URL,
+    baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL,
     socialProviders: {
         github: {
             clientId: env.GITHUB_CLIENT_ID,
